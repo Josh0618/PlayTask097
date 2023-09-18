684e544c615af3f33bb07ba5e1bd63014f779a33
Josh0618 <tehlianqing@gmail.com>
2023-09-18 15:31:32 +0800

    git add . git commit -m "添加了MainActivity2和设置TextView文本" git push origin master

--- app/src/main/java/com/jnu/student/MainActivity2.java
+++ app/src/main/java/com/jnu/student/MainActivity2.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.widget.TextView;
+
 public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // 设置布局
+
+        // 通过ID找到TextView
+        TextView helloTextView = findViewById(R.id.text_view_hello_world);
+
+        // 通过资源名称获取字符串资源并设置给TextView
+        String greeting = getResources().getString(R.string.hello_andriod);
+        helloTextView.setText(greeting);
     }
 }
