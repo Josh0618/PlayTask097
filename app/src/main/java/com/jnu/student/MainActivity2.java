753bb59bf0adec2b84b95396b0978515609297dc
Josh0618 <tehlianqing@gmail.com>
2023-09-21 22:28:41 +0800

    git add . git commit -m "添加了MainActivity2和设置TextView文本" git push origin master

--- app/src/main/java/com/jnu/student/MainActivity2.java
+++ app/src/main/java/com/jnu/student/MainActivity2.java
@@ -2,12 +2,16 @@ package com.jnu.student;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    private TextView textView;
+    private TextView textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +23,24 @@ public class MainActivity2 extends AppCompatActivity {
         // 通过资源名称获取字符串资源并设置给TextView
         String greeting = getResources().getString(R.string.hello_andriod);
         helloTextView.setText(greeting);
+        textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+    }
+    public void swapText(View view) {
+        String text1 = textView.getText().toString();
+        String text2 = textView2.getText().toString();
+
+        textView.setText(text2);
+        textView2.setText(text1);
+
+        // 显示Toast
+        Toast.makeText(this, "交换成功", Toast.LENGTH_SHORT).show();
+
+        // 显示AlertDialog
+        new AlertDialog.Builder(this)
+                .setTitle("交换成功")
+                .setMessage("文本已成功交换")
+                .setPositiveButton("确定", null)
+                .show();
     }
 }
