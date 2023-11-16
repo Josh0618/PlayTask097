a3c3e3110dba2b8c8c386b75f5975f4502297ce6
Josh0618 <tehlianqing@gmail.com>
2023-11-16 15:30:31 +0800

    git add . git commit -m "添加了MainActivity2和设置TextView文本" git push origin master

--- app/src/main/java/com/jnu/student/MainActivity2.java
+++ app/src/main/java/com/jnu/student/MainActivity2.java
@@ -1,55 +1,107 @@
 package com.jnu.student;
 
-import static com.jnu.student.R.id.recycler_view;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.annotation.SuppressLint;
-import android.app.AlertDialog;
-import android.os.Bundle;
-
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
+
     private RecyclerView recyclerView;
-    private BookAdapter adapter;
+    private List<Book> books;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this)); // 设置布局管理器，例如 LinearLayoutManager
+        // 获取书籍列表
+        books = getListBooks();
 
-        BookListMainActivity bookListMainActivity = new BookListMainActivity();
-        List<Book> bookList = bookListMainActivity.getListBooks();
-
-        adapter = new BookAdapter(bookList);
+        // 初始化 RecyclerView
+        recyclerView = findViewById(R.id.recycle_view_books);
+        RecyclerViewBookAdapter adapter = new RecyclerViewBookAdapter(books);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
     }
-    /*public void swapText(View view) {
-        String text1 = textView.getText().toString();
-        String text2 = textView2.getText().toString();
 
-        textView.setText(text2);
-        textView2.setText(text1);
-
-        // 显示Toast
-        Toast.makeText(this, "交换成功", Toast.LENGTH_SHORT).show();
-
-        // 显示AlertDialog
-        new AlertDialog.Builder(this)
-                .setTitle("交换成功")
-                .setMessage("文本已成功交换")
-                .setPositiveButton("确定", null)
-                .show();
-    }*/
+    // 模拟书籍列表数据
+    public List<Book> getListBooks() {
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(new Book("软件项目管理案例教程（第4版）", R.drawable.book_2));
+        bookList.add(new Book("创新工程实践", R.drawable.book_no_name));
+        bookList.add(new Book("信息安全数学基础（第2版）", R.drawable.book_1));
+        return bookList;
+    }
 }
+
+class Book {
+    private String title;
+    private int coverResourceId;
+
+    public Book(String title, int coverResourceId) {
+        this.title = title;
+        this.coverResourceId = coverResourceId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getCoverResourceId() {
+        return coverResourceId;
+    }
+}
+
+class RecyclerViewBookAdapter extends RecyclerView.Adapter<RecyclerViewBookAdapter.BookViewHolder> {
+
+    private List<Book> bookList;
+
+    public RecyclerViewBookAdapter(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    @NonNull
+    @Override
+    public BookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_book, parent, false);
+        return new BookViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BookViewHolder holder, int position) {
+        Book book = bookList.get(position);
+        holder.bookTitle.setText(book.getTitle());
+        holder.bookCover.setImageResource(book.getCoverResourceId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return bookList.size();
+    }
+
+    public static class BookViewHolder extends RecyclerView.ViewHolder {
+        ImageView bookCover;
+        TextView bookTitle;
+
+        public BookViewHolder(@NonNull View itemView) {
+            super(itemView);
+            bookCover = itemView.findViewById(R.id.image_view_book_cover);
+            bookTitle = itemView.findViewById(R.id.text_view_book_title);
+        }
+    }
+}
+
+
+
