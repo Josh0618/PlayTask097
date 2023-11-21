3e047d3fe5f3cc61ccf12cf47f0ab279f64e26f4
Josh0618 <tehlianqing@gmail.com>
2023-11-21 14:41:57 +0800

    git add . git commit -m "添加了MainActivity2和设置TextView文本" git push origin master

--- app/src/main/java/com/jnu/student/MainActivity2.java
+++ app/src/main/java/com/jnu/student/MainActivity2.java
@@ -1,22 +1,11 @@
 package com.jnu.student;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.GestureDetector;
 import android.view.LayoutInflater;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -26,171 +15,95 @@ import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private View emptyStateLayout;
-    private List<Book> books = new ArrayList<>();
-    private RecyclerViewBookAdapter adapter;
-    ActivityResultLauncher<Intent> launcher;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 获取书籍列表
-        books = getListBooks();
 
-        emptyStateLayout = findViewById(R.id.emptyStateLayout);
+        RecyclerView mainRecyclerView = findViewById(R.id.recycle_view_books);
+        mainRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        recyclerView = findViewById(R.id.recycle_view_books);
-        adapter = new RecyclerViewBookAdapter(books);
-        registerForContextMenu(recyclerView);
-        RecyclerViewBookAdapter adapter = new RecyclerViewBookAdapter(books);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
-        recyclerView.requestFocus();
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book("软件项目管理案例教程（第4版）", R.drawable.book_2));
+        books.add(new Book("创新工程实践", R.drawable.book_no_name));
+        books.add(new Book("信息安全数学基础（第2版）", R.drawable.book_1));
 
+        BookAdapter bookAdapter = new BookAdapter(books);
+        mainRecyclerView.setAdapter(bookAdapter);
 
-        if (books.isEmpty()) {
-            recyclerView.setVisibility(View.GONE);
-            emptyStateLayout.setVisibility(View.VISIBLE);
-        } else {
-            recyclerView.setVisibility(View.VISIBLE);
-            emptyStateLayout.setVisibility(View.GONE);
+    }
+
+    public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
+        private List<Book> bookList;
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            private final TextView textViewName;
+            private final ImageView imageViewItem;
+
+            public ViewHolder(View bookView) {
+                super(bookView);
+
+                textViewName = bookView.findViewById(R.id.text_view_book_title);
+                imageViewItem = bookView.findViewById(R.id.image_view_book_cover);
+            }
+
+            public TextView getTextViewName() {
+                return textViewName;
+            }
+
+            public ImageView getImageViewItem() {
+                return imageViewItem;
+            }
+        }
+        public BookAdapter(List<Book> bookList) {
+            this.bookList = bookList;
         }
 
-
-        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
-            GestureDetector gestureDetector = new GestureDetector(MainActivity2.this, new GestureDetector.SimpleOnGestureListener() {
-                @Override
-                public void onLongPress(MotionEvent e) {
-                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
-                    if (child != null) {
-                        // Show context menu
-                        int position = recyclerView.getChildAdapterPosition(child);
-                        if (position != RecyclerView.NO_POSITION) {
-                            openContextMenu(child);
-                        }
-                    }
-                }
-            });
-
-            @Override
-            public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
-                gestureDetector.onTouchEvent(e);
-                return false;
-            }
-
-            @Override
-            public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
-            }
-
-            @Override
-            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-            }
-        });
-
-        registerForContextMenu(recyclerView);
-
-    }
-
-    public List<Book> getListBooks() {
-        List<Book> bookList = new ArrayList<>();
-        bookList.add(new Book("软件项目管理案例教程（第4版）", R.drawable.book_2));
-        bookList.add(new Book("创新工程实践", R.drawable.book_no_name));
-        bookList.add(new Book("信息安全数学基础（第2版）", R.drawable.book_1));
-        return bookList;
-    }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.context_menu, menu);
-    }
-
-    @Override
-    public boolean onContextItemSelected(@NonNull MenuItem item) {
-        int itemId = item.getItemId();
-
-        if (itemId == R.id.add_item) {
-
-            return true;
-        } else if (itemId == R.id.edit_item) {
-
-            return true;
-        } else if (itemId == R.id.delete_item) {
-
-            return true;
-        } else {
-            return super.onContextItemSelected(item);
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_book, viewGroup, false);
+            return new ViewHolder(view);
         }
-    }
 
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, int position) {
+            viewHolder.getTextViewName().setText(bookList.get(position).getTitle());
+            viewHolder.getImageViewItem().setImageResource(bookList.get(position).getCoverResourceId());
+        }
+
+        @Override
+        public int getItemCount() {
+            return bookList.size();
+        }
+
+    }
 }
 
 class Book {
-    private String title;
-    private int coverResourceId;
-
-    public Book(String title, int coverResourceId) {
-        this.title = title;
-        this.coverResourceId = coverResourceId;
+    public int getCoverResourceId() {
+        return imageResourceId;
     }
+
+    private int imageResourceId;
 
     public String getTitle() {
-        return title;
+        return name;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+    private String name;
 
-    public int getCoverResourceId() {
-        return coverResourceId;
-    }
-}
-
-class RecyclerViewBookAdapter extends RecyclerView.Adapter<RecyclerViewBookAdapter.BookViewHolder> {
-
-    private List<Book> bookList;
-
-    public RecyclerViewBookAdapter(List<Book> bookList) {
-        this.bookList = bookList;
-    }
-
-    @NonNull
-    @Override
-    public BookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_book, parent, false);
-
-        return new BookViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull BookViewHolder holder, int position) {
-        Book book = bookList.get(position);
-        holder.bookTitle.setText(book.getTitle());
-        holder.bookCover.setImageResource(book.getCoverResourceId());
-    }
-
-    @Override
-    public int getItemCount() {
-        return bookList.size();
-    }
-
-    public static class BookViewHolder extends RecyclerView.ViewHolder {
-        ImageView bookCover;
-        TextView bookTitle;
-
-        public BookViewHolder(@NonNull View itemView) {
-            super(itemView);
-            bookCover = itemView.findViewById(R.id.image_view_book_cover);
-            bookTitle = itemView.findViewById(R.id.text_view_book_title);
-        }
-
+    public Book(String name_, int imageResourceId_) {
+        this.name=name_;
+        this.imageResourceId =imageResourceId_;
     }
 }
+
+
+
+
+
+
 
 
 
